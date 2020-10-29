3627f6fd862ae1bcd9272e9e4d8bd40f725ec36e
xuzhen <1712954704@qq.com>
2020-10-29 17:54:27 +0800

    'bug'

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,35 +1,50 @@
 <template>
-    <div>
-        <template v-for="(item) in list">
-            <template v-if="item.children">
-                <el-submenu index="item.index" :key="item.index">
-                    <i class="el-icon-location"></i>
-                    <span>{{item.text}}</span>
-                    <el-menu-item-group>
-                        <navList :list="item.children"></navList>
-                    </el-menu-item-group>
-                </el-submenu>
-            </template>
-            <template v-else>
-                <el-menu-item index="item.index" :key="item.index">{{item.text}}</el-menu-item>
-            </template>
-        </template>
-    </div>
+  <div class="navList">
+    <template v-for="item in list">
+      <template v-if="item.children == null">
+        <el-submenu index="item.index" :key="item.index">
+          <template slot="title">
+            <i class="el-icon-location"></i>
+            <span>1{{ item.text }}</span>
+          </template>
+            <el-menu-item index="5-1">
+                <i class="el-icon-setting"></i>
+                <span slot="title">选项2-1</span>
+            </el-menu-item>
+            
+            <el-menu-item index="5-1">
+                <i class="el-icon-setting"></i>
+                <span slot="title">选项2-1</span>
+            </el-menu-item>
+            
+          <!-- <el-menu-item-group> -->
+            <!-- <navList :list="item.children"></navList> -->
+          <!-- </el-menu-item-group> -->
+        </el-submenu>
+      </template>
+      <template v-else>
+        <el-menu-item index="item.index" :key="item.index">
+          <i class="el-icon-setting"></i>
+          <span slot="title">2{{ item.text }}</span>
+        </el-menu-item>
+      </template>
+    </template>
+  </div>
 </template>
 <script>
 export default {
-    name: "navList",
-    data() {
-        return {};
-    },
-    props: {
-        list:{
-            type: Array,
-            default() {
-                return [];
-            },
-            required: true
-        }
+  name: "navList",
+  data() {
+    return {};
+  },
+  props: {
+    list: {
+      type: Array,
+      default() {
+        return [];
+      },
+      required: true
     }
-}
+  }
+};
 </script>
--- src/components/admin/Sidebar.vue
+++ src/components/admin/Sidebar.vue
@@ -10,8 +10,47 @@
       text-color="#fff"
       active-text-color="#ffd04b"
       menu-trigger="hover"
+      :collapse="collapse"
     >
-        <navList :list="list"></navList>
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>导航一已</span>
+        </template>
+        <el-menu-item-group>
+          <template slot="title">分组一</template>
+          <el-menu-item index="1-1">选项1</el-menu-item>
+          <el-menu-item index="1-2">选项2</el-menu-item>
+        </el-menu-item-group>
+        <el-menu-item-group title="分组2">
+          <el-menu-item index="1-3">选项3</el-menu-item>
+        </el-menu-item-group>
+        <el-submenu index="1-4">
+          <template slot="title">选项4</template>
+          <el-menu-item index="1-4-1">选项1</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+      <el-submenu index="2">
+          <template slot="title">
+            <i class="el-icon-menu"></i>
+            <span slot="title">导航二</span>
+          </template>
+          <!-- <template slot="title">分组一</template> -->
+          <el-menu-item index="1-1">
+            <i class="el-icon-setting"></i>
+            <span slot="title">选项2-1</span>
+          </el-menu-item>
+          <el-menu-item index="1-2">选项2-2</el-menu-item>
+      </el-submenu>
+      <el-menu-item index="3" disabled>
+        <i class="el-icon-document"></i>
+        <span slot="title">导航三</span>
+      </el-menu-item>
+      <el-menu-item index="4">
+        <i class="el-icon-setting"></i>
+        <span slot="title">导航四导航</span>
+      </el-menu-item>
+      <navList :list="list"></navList>
     </el-menu>
   </div>
 </template>
@@ -19,120 +58,110 @@
 import navList from "@/components/Nav.vue";
 export default {
   name: "Sidebar",
-  components:{
-      "navList": navList
+  components: {
+    navList: navList
   },
   data() {
     return {
-        list:[
+      collapse: false,
+      list: [
+        {
+          mouduleurl: "/home",
+          text: "后台管理",
+          value: "00010001",
+          index: "5",
+          children: [
             {
-                "mouduleurl": "/home",
-                "text": "后台管理",
-                "value": "00010001",
-                "index": "1-1",
-                                "children": [
-                    {
-                        "mouduleurl": "pages/config/config_list.html",
-                        "text": "系统参数",
-                        "value": "00010001",
-                        "index": "1-1",
-                    }, 
-                    {
-                        "mouduleurl": "pages/dmp/table_list.html",
-                        "text": "数据表结构",
-                        "value": "00010002",
-                        "index": "1-2",
-                        "children": [
-                            {
-                                "mouduleurl": "pages/config/config_list.html",
-                                "text": "数据表关联管理",
-                                "value": "00010002001",
-                                "index": "1-2-1",
-                            }
-                        ]
-                    },
-                    {
-                        "mouduleurl": "pages/module/module_settinglist.html",
-                        "text": "模块地址",
-                        "value": "00010003",
-                        "index": "1-3",
-                    }
-                ]
-            }
-        ],
-        itemlist: [
-            {
-                "mouduleurl": "/home",
-                "text": "后台管理",
-                "value": "0001",
-                "index": "1",
-                "children": [
-                    {
-                        "mouduleurl": "pages/config/config_list.html",
-                        "text": "系统参数",
-                        "value": "00010001",
-                        "index": "1-1",
-                    }, 
-                    {
-                        "mouduleurl": "pages/dmp/table_list.html",
-                        "text": "数据表结构",
-                        "value": "00010002",
-                        "index": "1-2",
-                        "children": [
-                            {
-                                "mouduleurl": "pages/config/config_list.html",
-                                "text": "数据表关联管理",
-                                "value": "00010002001",
-                                "index": "1-2-1",
-                            }
-                        ]
-                    },
-                    {
-                        "mouduleurl": "pages/module/module_settinglist.html",
-                        "text": "模块地址",
-                        "value": "00010003",
-                        "index": "1-3",
-                    }
-                ],
- 
-            }, 
-            {
-                "mouduleurl": "",
-                "text": "文章管理",
-                "value": "0002",
-                "index": "2",
-                "children": [
-                    {
-                        "mouduleurl": "pages/article/article_list.html",
-                        "text": "文章列表",
-                        "value": "00020001",
-                        "index": "2-1",
-                    }
-                ],
- 
+              mouduleurl: "pages/config/config_list.html",
+              text: "系统参数",
+              value: "00010001",
+              index: "5-1"
             },
             {
-                "mouduleurl": "",
-                "text": "爬虫专题",
-                "value": "0003",
-                "index": "3",
-                "children": [
-                    {
-                        "mouduleurl": "pages/spider/music/music_search.html",
-                        "text": "网易云音乐",
-                        "value": "00030001",
-                        "index": "3-1",
-                    }
-                ],
- 
-            }, 
+              mouduleurl: "pages/dmp/table_list.html",
+              text: "数据表结构",
+              value: "00010002",
+              index: "5-2"
+            },
             {
-                "mouduleurl": "",
-                "text": "测试4",
-                "value": "0004",
-                "index": "4",
+              mouduleurl: "pages/module/module_settinglist.html",
+              text: "模块地址V",
+              value: "00010003",
+              index: "5-3"
             }
-        ],
+          ]
+        }
+      ],
+      itemlist: [
+        {
+          mouduleurl: "/home",
+          text: "后台管理",
+          value: "0001",
+          index: "1",
+          children: [
+            {
+              mouduleurl: "pages/config/config_list.html",
+              text: "系统参数",
+              value: "00010001",
+              index: "1-1"
+            },
+            {
+              mouduleurl: "pages/dmp/table_list.html",
+              text: "数据表结构",
+              value: "00010002",
+              index: "1-2",
+              children: [
+                {
+                  mouduleurl: "pages/config/config_list.html",
+                  text: "数据表关联管理",
+                  value: "00010002001",
+                  index: "1-2-1"
+                }
+              ]
+            },
+            {
+              mouduleurl: "pages/module/module_settinglist.html",
+              text: "模块地址",
+              value: "00010003",
+              index: "1-3"
+            }
+          ]
+        },
+        {
+          mouduleurl: "",
+          text: "文章管理",
+          value: "0002",
+          index: "2",
+          children: [
+            {
+              mouduleurl: "pages/article/article_list.html",
+              text: "文章列表",
+              value: "00020001",
+              index: "2-1"
+            }
+          ]
+        },
+        {
+          mouduleurl: "",
+          text: "爬虫专题",
+          value: "0003",
+          index: "3",
+          children: [
+            {
+              mouduleurl: "pages/spider/music/music_search.html",
+              text: "网易云音乐",
+              value: "00030001",
+              index: "3-1"
+            }
+          ]
+        },
+        {
+          mouduleurl: "",
+          text: "测试4",
+          value: "0004",
+          index: "4"
+        }
+      ]
     };
   },
   methods: {
